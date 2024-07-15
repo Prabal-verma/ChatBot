cd523a3abab371663b5fe5a390487b7a4ffbba83
Prabal Verma <prableverma@gmail.com>
2024-07-15 16:23:14 +0530

    test

--- src/routes/sign-up.jsx
+++ src/routes/sign-up.jsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/clerk-react";
 export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <SignUp path="/sign-up" signInUrl="/sign-in" signInFallbackRedirectUrl={"./sign-in"} signInForceRedirectUrl={"./sign-in"}/>
+      <SignUp path="/sign-up" signInUrl="/sign-in" signInFallbackRedirectUrl={"./"} signInForceRedirectUrl={"./sign-in"}/>
     </div>
   );
 }
